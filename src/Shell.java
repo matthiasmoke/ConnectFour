d90877b48d70a4be1668c6a56c4d5f6e69af3341
matthiasmoke <mrauch593@googlemail.com>
2018-11-30 00:42:59 +0100

    Started implementing input evaluation and added interface and classes

--- src/Shell.java
+++ src/Shell.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public final class Shell {
 
     private static final String DEFAULT_ERR_MESSAGE
@@ -9,6 +14,8 @@ public final class Shell {
                     "new", "level", "switch", "move", "witness", "print",
                     "help", "quit"
             };
+
+    private static boolean run;
 
 
     private Shell() {
@@ -44,4 +51,105 @@ public final class Shell {
         }
     }
 
+    /**
+     * Evals the input
+     * @param input user input
+     */
+    private static void evalInput(String input) {
+        Scanner sc =  new Scanner(input);
+        String command = "";
+
+        if (sc.hasNext()) {
+            command = sc.next();
+
+            switch (command.charAt(0)) {
+                case 'n':
+                    if (command.equals(COMMANDS[0]) || command.length() == 1) {
+
+                    }
+                    break;
+
+                case 'l':
+                    if (command.equals(COMMANDS[1]) || command.length() == 1) {
+                        int i = 0;
+
+                        if (sc.hasNextInt()) {
+                            i = sc.nextInt();
+                        }
+
+                        setLevel(i);
+                    }
+                    break;
+
+                case 's':
+                    if (command.equals(COMMANDS[2]) || command.length() == 1) {
+
+                    }
+
+                    break;
+                case 'm':
+                    if (command.equals(COMMANDS[3]) || command.length() == 1) {
+                        int c = 0;
+
+                        if (sc.hasNextInt()) {
+                            c = sc.nextInt();
+                        }
+
+                        move(c);
+                    }
+
+                    break;
+                case 'w':
+                    if (command.equals(COMMANDS[4]) || command.length() == 1) {
+
+                    }
+
+                    break;
+                case 'p':
+                    if (command.equals(COMMANDS[5]) || command.length() == 1) {
+
+                    }
+
+                    break;
+                case 'h':
+                    if (command.equals(COMMANDS[6]) || command.length() == 1) {
+                        printHelpMessage();
+                    }
+                    break;
+
+                case 'q':
+                    if (command.equals(COMMANDS[7]) || command.length() == 1) {
+                        run = false;
+                    }
+                    break;
+
+                default:
+                    System.out.println(DEFAULT_ERR_MESSAGE);
+            }
+        }
+
+        sc.close();
+    }
+
+    private static void printHelpMessage() {
+        StringBuilder b = new StringBuilder();
+
+        System.out.println(b.toString());
+    }
+
+    private static void move(int c) {
+        if (c > 0) {
+
+        } else {
+            System.out.println(DEFAULT_ERR_MESSAGE);
+        }
+    }
+
+    private static void setLevel(int i) {
+        if (i > 0 && i < 6) {
+
+        } else {
+            System.out.println(DEFAULT_ERR_MESSAGE);
+        }
+    }
 }
